0384bd43b061231fd9faaa11992447048ce996c5
Zic <logzcx@126.com>
2017-07-07 23:43:40 +0800

    完成了对格子的抽象。

--- main.js
+++ main.js
@@ -1,8 +1,48 @@
+//随机函数 a,b为整数 [a,b]
+function random(a, b) {
+
+	return a + Math.floor(Math.random() * (b - a + 1));
+}
+
+
+//形状类
+function Shape() {
+
+	this.x = 0;
+	this.y = 0;
+
+	//类别。
+	this.type = 0;
+
+	//颜色
+	this.color = 0;
+
+}
+
+
+//小方块类，构成舞台的最基本元素。
+function Grid() {
+
+	this.x = 0;
+	this.y = 0;
+	this.color = 0;
+
+	this.$dom = $("<span></span>");
+}
+
+Grid.colors = ["", "orange", "olive", "cornflowerblue", "cyan", "red"];
+
+Grid.prototype.refresh = function () {
+	this.$dom.removeAttr("class");
+	this.$dom.addClass(Grid.colors[this.color]);
+}
+
+//游戏类，总控全局的。
 function Game() {
 
 	this.$stage = $(".stage");
 
-	this.$grids = [];
+	this.grids = [];
 
 	this.width = 13;
 	this.height = 20;
@@ -10,30 +50,47 @@ function Game() {
 }
 
 
-//将自身属性 $grids 填充完毕
+//将自身属性 grids 填充完毕
 Game.prototype.fillGrids = function () {
 	for (var row = 0; row < this.height; row++) {
 
-		var $tempRow = [];
+		var tempRow = [];
 
 		for (var column = 0; column < this.width; column++) {
-			var $grid = $("<span class='grid'></span>");
-			$tempRow.push($grid);
+			var grid = new Grid();
+
+			//grid.color = random(0, Grid.colors.length - 1);
+
+			tempRow.push(grid);
 		}
 
-		this.$grids.push($tempRow)
+		this.grids.push(tempRow)
 	}
 
 }
 
 
-//将 $grids 附着到 $stage上去 准备舞台
+//将 grids 附着到 $stage上去 准备舞台
 Game.prototype.prepareStage = function () {
 
 	for (var row = 0; row < this.height; row++) {
 		for (var col = 0; col < this.width; col++) {
 
-			this.$stage.append(this.$grids[row][col]);
+			var grid = this.grids[row][col];
+			this.$stage.append(grid.$dom);
+
+		}
+	}
+}
+
+//刷新画布上的颜色
+Game.prototype.refreshStage = function () {
+
+	for (var row = 0; row < this.height; row++) {
+		for (var col = 0; col < this.width; col++) {
+
+			var grid = this.grids[row][col];
+			grid.refresh();
 
 		}
 	}
@@ -47,7 +104,7 @@ Game.prototype.init = function () {
 
 	this.prepareStage();
 
-
+	this.refreshStage();
 }
 
 
