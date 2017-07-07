b69899183f22150d9c15093608aa5b79f2db8fd9
Zic <logzcx@126.com>
2017-07-08 00:54:45 +0800

    现在形状已经可以自由掉落了。

--- main.js
+++ main.js
@@ -4,6 +4,9 @@ function random(a, b) {
 	return a + Math.floor(Math.random() * (b - a + 1));
 }
 
+WIDTH_NUM = 13;
+HEIGH_NUM = 20;
+
 function Direction() {
 }
 Direction.UP = 0;
@@ -33,6 +36,41 @@ function Shape(game) {
 }
 
 
+//将当前的形状全部置为一种category
+Shape.prototype.setCategory = function (cate) {
+
+	//把之前的shap占用的grid全部置为category=0;
+	for (var i = 0; i < this.coords.length; i++) {
+		var coord = this.coords[i];
+		var x = coord[0];
+		var y = coord[1];
+		var grid = this.game.grids[x][y];
+		grid.category = cate;
+
+		if (cate == 0) {
+			grid.color = 0;
+		}
+
+	}
+
+}
+
+
+//往下面掉一格
+Shape.prototype.dropOneStep = function () {
+
+	this.setCategory(0);
+
+	this.y++;
+
+	if (this.y == 20) {
+		this.y = 19;
+	}
+
+	this.setCategory(2);
+
+}
+
 //类别2 L1型
 //类别3 L2型
 //类别4 Z1型
@@ -47,19 +85,23 @@ Shape.prototype.refreshCoords = function () {
 		//上下方向
 		if (this.direction == Direction.UP || this.direction == Direction.DOWN) {
 
-			this.coords = [];
-			this.coords.push([this.x, this.y - 1]);
-			this.coords.push([this.x, this.y]);
-			this.coords.push([this.x, this.y + 1]);
-			this.coords.push([this.x, this.y + 2]);
+			if (this.y + 2 < HEIGH_NUM) {
+				this.coords = [];
+				this.coords.push([this.x, this.y - 1]);
+				this.coords.push([this.x, this.y]);
+				this.coords.push([this.x, this.y + 1]);
+				this.coords.push([this.x, this.y + 2]);
+			}
 
 		} else if (this.direction == Direction.LEFT || this.direction == Direction.RIGHT) {
 
-			this.coords = [];
-			this.coords.push([this.x - 1, this.y]);
-			this.coords.push([this.x, this.y]);
-			this.coords.push([this.x + 1, this.y]);
-			this.coords.push([this.x + 2, this.y]);
+			if (this.x - 1 >= 0 && this.x + 2 < WIDTH_NUM) {
+				this.coords = [];
+				this.coords.push([this.x - 1, this.y]);
+				this.coords.push([this.x, this.y]);
+				this.coords.push([this.x + 1, this.y]);
+				this.coords.push([this.x + 2, this.y]);
+			}
 
 		}
 
@@ -77,15 +119,11 @@ Shape.prototype.refresh = function () {
 		var x = coord[0];
 		var y = coord[1];
 
-		console.log("x:" + x)
-		console.log("y:" + y)
 
 		var grids = this.game.grids;
 
 		var grid = grids[x][y];
 
-		console.log("grid.x:" + grid.x)
-		console.log("grid.y:" + grid.y)
 
 		grid.color = this.color;
 
@@ -106,6 +144,9 @@ function Grid() {
 
 	this.color = 0;
 
+	//0:地板，啥都不是。 1: 岩石，固定不动了。 2: 形状中的grid
+	this.category = 0;
+
 	this.$dom = $("<span></span>");
 }
 
@@ -123,27 +164,22 @@ function Game() {
 
 	this.grids = [];
 
-	this.width = 13;
-	this.height = 20;
+	this.width = WIDTH_NUM;
+	this.height = HEIGH_NUM;
 
 }
 
 
 //将自身属性 grids 填充完毕
 Game.prototype.fillGrids = function () {
-	for (var x = 0; x < this.width; x++) {
+	for (var x = 0; x < WIDTH_NUM; x++) {
 
 		var xArr = [];
 
-		for (var y = 0; y < this.height; y++) {
+		for (var y = 0; y < HEIGH_NUM; y++) {
 			var grid = new Grid();
 			grid.x = x;
 			grid.y = y;
-
-			if (x == 2 && y == 2) {
-				grid.color = 2;
-			}
-
 
 			xArr.push(grid);
 		}
@@ -158,11 +194,11 @@ Game.prototype.fillGrids = function () {
 Game.prototype.prepareStage = function () {
 
 
-	for (var x = 0; x < this.width; x++) {
-		for (var y = 0; y < this.height; y++) {
+	for (var x = 0; x < WIDTH_NUM; x++) {
+		for (var y = 0; y < HEIGH_NUM; y++) {
 			var grid = this.grids[x][y];
 
-			grid.$dom.css({"left": (x * grid.unit) + "px","top": (y * grid.unit) + "px"});
+			grid.$dom.css({"left": (x * grid.unit) + "px", "top": (y * grid.unit) + "px"});
 
 			this.$stage.append(grid.$dom);
 		}
@@ -174,8 +210,8 @@ Game.prototype.prepareStage = function () {
 //刷新画布上的颜色
 Game.prototype.refreshStage = function () {
 
-	for (var x = 0; x < this.width; x++) {
-		for (var y = 0; y < this.height; y++) {
+	for (var x = 0; x < WIDTH_NUM; x++) {
+		for (var y = 0; y < HEIGH_NUM; y++) {
 			var grid = this.grids[x][y];
 			grid.refresh();
 		}
@@ -209,8 +245,15 @@ $(function () {
 	shape.direction = Direction.UP;
 	shape.type = 0;
 
-	shape.refreshCoords();
-	shape.refresh();
+
+	setInterval(function () {
+
+		shape.dropOneStep();
+
+		shape.refreshCoords();
+		shape.refresh();
+	}, 1000);
+
 
 });
 
